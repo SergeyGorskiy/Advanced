54a5aecefade6f7fa78f8813ad8a76eb854eba7e
Sergey Nazhestkin <9497020@mail.ru>
2022-02-02 20:09:31 +0300

    Реализованы примеры создания форм для операций CRUD с использованием Blazor.

---
+++ Advanced.Web/Blazor/Forms/DeptStateValidator.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Advanced.Web.EF;
+using Advanced.Web.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Advanced.Web.Blazor.Forms
+{
+    public class DeptStateValidator : OwningComponentBase<DataContext>
+    {
+        public DataContext Context => Service;
+
+        [Parameter] 
+        public long DepartmentId { get; set; }
+
+        [Parameter] 
+        public string State { get; set; }
+
+        [CascadingParameter] 
+        public EditContext CurrentEditContext { get; set; }
+
+        private string DeptName { get; set; }
+
+        private IDictionary<long, string> LocationStates { get; set; }
+
+        private void Validate(Person model, ValidationMessageStore store)
+        {
+            if (model.DepartmentId == DepartmentId 
+                && (!LocationStates.ContainsKey(model.LocationId) 
+                    || LocationStates[model.LocationId] != State))
+            {
+                store.Add(CurrentEditContext.Field("LocationId"), $"{DeptName} staff must be in: {State}");
+            }
+            else
+            {
+                store.Clear();
+            }
+        }
+
+        protected override void OnInitialized()
+        {
+            ValidationMessageStore store = new ValidationMessageStore(CurrentEditContext);
+
+            CurrentEditContext.OnFieldChanged += (sender, args) =>
+            {
+                string name = args.FieldIdentifier.FieldName;
+                if (name == "DepartmentId" || name == "LocationId")
+                {
+                    Validate(CurrentEditContext.Model as Person, store);
+                }
+            };
+        }
+
+        protected override void OnParametersSet()
+        {
+            DeptName = Context.Departments.Find(DepartmentId).Name;
+            LocationStates = Context.Locations.ToDictionary(l => l.LocationId, l => l.State);
+        }
+    }
+}
--- Advanced.Web/Models/Person.cs
+++ Advanced.Web/Models/Person.cs
@@ -1,5 +1,4 @@
-﻿using System.Reflection.Metadata.Ecma335;
-using Microsoft.AspNetCore.Components;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Advanced.Web.Models
 {
@@ -7,12 +6,21 @@ namespace Advanced.Web.Models
     {
         public long PersonId { get; set; }
 
+        [Required(ErrorMessage = "A firstname is required")]
+        [MinLength(3, ErrorMessage = "Firstname mus be 3 or more characters")]
         public string Firstname { get; set; }
 
+
+        [Required(ErrorMessage = "A surname is required")]
+        [MinLength(3, ErrorMessage = "Surname mus be 3 or more characters")]
         public string Surname { get; set; }
 
+        [Required]
+        [Range(1, long.MaxValue, ErrorMessage = "A department most be selected")]
         public long DepartmentId { get; set; }
 
+        [Required]
+        [Range(1, long.MaxValue, ErrorMessage = "A location most be selected")]
         public long LocationId { get; set; }
 
         public Department Department { get; set; }
